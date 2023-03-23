5594ebe2d0209ea7a9a1f9071ef25a1649ca9ad5
Jacob Biwott <jacobbiwott@yahoo.com>
2023-03-23 05:39:12 -0700

    print statement with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,14 @@
 
 /**
  *main - print "with proper grammer, but the outcome is
- *       a piece of art," new line
+ *       a piece of art,"
  *
  *return 0
  */
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,/n");
 
-return(0);
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
 }
