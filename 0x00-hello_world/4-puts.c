b7a49e0280ede30a54610b179487c82ed1d45e0a
Jacob Biwott <jacobbiwott@yahoo.com>
2023-03-23 00:56:06 -0700

    print statent using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  *main-begin of program
- *return value always (0)
  */
 int main (void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
+/**
+ *return value to main 0
+ */
 return (0);
 }
