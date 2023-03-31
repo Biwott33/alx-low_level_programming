e7f1bca719fe6870774d0dd4363e89d474f6b7fa
Jacob Biwott <jacobbiwott@yahoo.com>
2023-03-31 09:00:19 -0700

    comapre two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * _strcmp - Write a function that compares two strings.
- *
- * @s1: This is the input string
- * @s2: This is the input string
- *
- * Return: If the strings are equals return "0", if not return other number
+ * _strcmp - compares  strings.
+ * @s1: string to be compared.
+ * @s2: string for comparation.
+ * Return: Difference.
  */
 
 int _strcmp(char *s1, char *s2)
 {
-for ((*s1 != '\0' && *s2 != '\0') && (*s1 == *s2));
-
-s1++, s2++;
-		;
+while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+{
+s1++;
+s2++;
+}
 if (*s1 == *s2)
 {
 return (0);
 }
 else
-   
-return ("dosent match");
+{
+return (*s1 - *s2);
+}
 }
