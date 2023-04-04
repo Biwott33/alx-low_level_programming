663fc4c7a332fd281ccde39a6515bb135e9ecaf6
Jacob Biwott <jacobbiwott@yahoo.com>
2023-04-04 06:46:04 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - print the aphlabet 10 times
+  *
+  *return: nill
+  */
+
+void print_alphabet_x10(void);
+
+{
+int a;
+char b;
+
+for (a = 0; a < 10; ++a)
+{
+for (b = 'a'; c <= 'z'; c++)
+{
+_putchar(b);
+}
+_putchar('/n');
+}
+}
